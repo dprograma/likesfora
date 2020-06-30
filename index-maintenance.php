422c308f5f7bed73c1bfe5b42371d50b7681589b
dprograma <kenegwuda33@gmail.com>
2020-06-30 01:38:15 +0100

    first commit

---
+++ index-maintenance.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<html>
+
+<head>
+<style>
+    body{
+        background-color: #56c8e2;
+    }
+    img{
+        position: absolute;
+        margin: auto;
+        left: 440px;
+        top: 200px;
+    }
+    p{
+        font-size: 60px;
+        color:antiquewhite;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-weight: bolder;
+        text-align: center;
+        top: 80px;
+    }
+</style>
+</head>
+
+<body>
+    <div>
+        <p>
+            Website Under Construction
+        </p>
+    </div>
+    <div>
+        <img src="assets/images/maintenance.png" height="350" width="400">
+    </div>
+</body>
+
+</html>
---
+++ index.js
@@ -0,0 +1,4 @@
+var jquery = require('jquery');
+var bootstrap = require('bootstrap');
+var fontawesome = require('./node_modules/fontawesome/js/fontawesome.js');
+
